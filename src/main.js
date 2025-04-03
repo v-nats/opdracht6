4d0fded9b4b9bb1bb3144525d94d84f22c092711
stanvervecken <s152158@ap.be>
2025-04-03 11:36:42 +0200

    toevoegen sparkles

--- src/main.js
+++ src/main.js
@@ -1,137 +1,203 @@
-import './style.css'
-window.requestAnimFrame=function(){return window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.oRequestAnimationFrame||window.msRequestAnimationFrame||function(a){window.setTimeout(a,1E3/60)}}();
+import './style.css';
 
-document.onselectstart = function() {
+window.requestAnimFrame = (function () {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    }
+  );
+})();
+
+document.onselectstart = function () {
   return false;
 };
-var c = document.getElementById('c');
-var ctx = c.getContext('2d');
-var dpr = window.devicePixelRatio;
-var cw = window.innerWidth;
-var ch = window.innerHeight;
+
+const c = document.getElementById('c');
+const ctx = c.getContext('2d');
+const dpr = window.devicePixelRatio;
+const cw = window.innerWidth;
+const ch = window.innerHeight;
+
 c.width = cw * dpr;
 c.height = ch * dpr;
 ctx.scale(dpr, dpr);
-var rand = function(rMi, rMa){return ~~((Math.random()*(rMa-rMi+1))+rMi);}
+
+const rand = (rMi, rMa) => ~~((Math.random() * (rMa - rMi + 1)) + rMi);
 ctx.lineCap = 'round';
-var orbs = [];
-var orbCount = 30;
-var radius;
 
-var trailCB = document.getElementById('trail');
-var trail = trailCB.checked;
-var clearer = document.getElementById('clear');
+let orbs = [];
+let particles = [];
+let isDragging = false;
+const trailCB = document.getElementById('trail');
+let trail = trailCB.checked;
+const clearer = document.getElementById('clear');
 
-function createOrb(mx,my){
-  var dx = (cw/2) - mx;
-	var dy = (ch/2) - my;
-	var dist = Math.sqrt(dx * dx + dy * dy);
-	var angle = Math.atan2(dy, dx);
-	orbs.push({
-		x: mx,
-		y: my,
-		lastX: mx,
-		lastY: my,
-		hue: 0,
-		colorAngle: 0,
-		angle: angle + Math.PI/2,
-		//size: .5+dist/250,
-		size: rand(1,3)/2,
-		centerX: cw/2,
-		centerY: ch/2,		
-		radius: dist,
-		speed: (rand(5,10)/1000)*(dist/750)+.015,
-		alpha: 1 - Math.abs(dist)/cw,
-		draw: function() {			
-			ctx.strokeStyle = 'hsla('+this.colorAngle+',100%,50%,1)';	
-			ctx.lineWidth = this.size;			
-			ctx.beginPath();
-			ctx.moveTo(this.lastX, this.lastY);
-			ctx.lineTo(this.x, this.y);
-			ctx.stroke();
-		},	
-		update: function(){
-			var mx = this.x;
-			var my = this.y;	
-			this.lastX = this.x;
-			this.lastY = this.y;
-			var x1 = cw/2;
-			var y1 = ch/2;
-			var x2 = mx;
-			var y2 = my;		
-			var rise = y1-y2;
-			var run = x1-x2;
-			var slope = -(rise/run);
-			var radian = Math.atan(slope);
-			var angleH = Math.floor(radian*(180/Math.PI));		
-			if(x2 < x1 && y2 < y1){angleH += 180;}		
-			if(x2 < x1 && y2 > y1){angleH += 180;}		
-			if(x2 > x1 && y2 > y1){angleH += 360;}		
-			if(y2 < y1 && slope =='-Infinity'){angleH = 90;}		
-			if(y2 > y1 && slope =='Infinity'){angleH = 270;}		
-			if(x2 < x1 && slope =='0'){angleH = 180;}
-			if(isNaN(angleH)){angleH = 0;}
-			
-			this.colorAngle = angleH;
-			this.x = this.centerX + Math.sin(this.angle*-1) * this.radius;
-			this.y = this.centerY + Math.cos(this.angle*-1) * this.radius;
-			this.angle += this.speed;
-		
-		}
-	});
+function createOrb(mx, my) {
+  const dx = cw / 2 - mx;
+  const dy = ch / 2 - my;
+  const dist = Math.sqrt(dx * dx + dy * dy);
+  const angle = Math.atan2(dy, dx);
+  orbs.push({
+    x: mx,
+    y: my,
+    lastX: mx,
+    lastY: my,
+    colorAngle: 0,
+    angle: angle + Math.PI / 2,
+    size: rand(1, 3) / 2,
+    centerX: cw / 2,
+    centerY: ch / 2,
+    radius: dist,
+    speed: (rand(5, 10) / 1000) * (dist / 750) + 0.015,
+    draw: function () {
+      ctx.strokeStyle = `hsla(${this.colorAngle}, 100%, 50%, 1)`;
+      ctx.lineWidth = this.size;
+      ctx.beginPath();
+      ctx.moveTo(this.lastX, this.lastY);
+      ctx.lineTo(this.x, this.y);
+      ctx.stroke();
+
+      if (Math.random() < 0.1) {
+        for (let i = 0; i < 3; i++) {
+          createParticle(this.x + rand(-5, 5), this.y + rand(-5, 5), `hsla(${this.colorAngle}, 100%, 50%, 1)`);
+        }
+      }
+    },
+    update: function () {
+      this.lastX = this.x;
+      this.lastY = this.y;
+      const x1 = cw / 2;
+      const y1 = ch / 2;
+      const x2 = this.x;
+      const y2 = this.y;
+      const rise = y1 - y2;
+      const run = x1 - x2;
+      const slope = -(rise / run);
+      let radian = Math.atan(slope);
+      let angleH = Math.floor(radian * (180 / Math.PI));
+      if (x2 < x1 && y2 < y1) {
+        angleH += 180;
+      }
+      if (x2 < x1 && y2 > y1) {
+        angleH += 180;
+      }
+      if (x2 > x1 && y2 > y1) {
+        angleH += 360;
+      }
+      if (y2 < y1 && slope === '-Infinity') {
+        angleH = 90;
+      }
+      if (y2 > y1 && slope === 'Infinity') {
+        angleH = 270;
+      }
+      if (x2 < x1 && slope === '0') {
+        angleH = 180;
+      }
+      if (isNaN(angleH)) {
+        angleH = 0;
+      }
+
+      this.colorAngle = angleH;
+      this.x = this.centerX + Math.sin(this.angle * -1) * this.radius;
+      this.y = this.centerY + Math.cos(this.angle * -1) * this.radius;
+      this.angle += this.speed;
+    },
+  });
 }
 
-function orbGo(e){
-	var mx = e.pageX - c.offsetLeft;
-	var my = e.pageY - c.offsetTop;		
-	createOrb(mx,my);
+function createParticle(x, y, color) {
+  particles.push({
+    x,
+    y,
+    vx: rand(-2, 2),
+    vy: rand(-5, -1),
+    size: rand(0.1, 0.3),
+    color: color || `hsla(${rand(0, 360)}, 100%, 50%, 1)`,
+    life: 50,
+    draw: function () {
+      ctx.fillStyle = this.color;
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+      ctx.fill();
+    },
+    update: function () {
+      this.x += this.vx;
+      this.y += this.vy;
+      this.vy += 0.1;
+      this.life--;
+    },
+  });
 }
 
-function turnOnMove(){
-	c.addEventListener('mousemove', orbGo, false);	
+function handleMouseMove(e) {
+  const mx = e.pageX - c.offsetLeft;
+  const my = e.pageY - c.offsetTop;
+  if (isDragging) {
+    for (let i = 0; i < 5; i++) {
+      createParticle(mx, my);
+    }
+    createOrb(mx, my);
+  }
 }
 
-function turnOffMove(){
-	c.removeEventListener('mousemove', orbGo, false);	
+function handleMouseDown() {
+  isDragging = true;
 }
 
-function toggleTrails(){
-	trail = trailCB.checked;
+function handleMouseUp() {
+  isDragging = false;
 }
 
-function clear(){
- orbs = []; 
+function toggleTrails() {
+  trail = trailCB.checked;
 }
 
-c.addEventListener('mousedown', orbGo, false);
-c.addEventListener('mousedown', turnOnMove, false);
-c.addEventListener('mouseup', turnOffMove, false);
+function clearCanvas() {
+  orbs = [];
+  particles = [];
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, cw, ch);
+  initOrbs();
+}
+
+c.addEventListener('mousemove', handleMouseMove, false);
+c.addEventListener('mousedown', handleMouseDown, false);
+c.addEventListener('mouseup', handleMouseUp, false);
 trailCB.addEventListener('change', toggleTrails, false);
-clearer.addEventListener('click', clear, false);
+clearer.addEventListener('click', clearCanvas, false);
 
-var count = 100;
-while(count--){
-		createOrb(cw/2, ch/2+(count*2));
+function initOrbs() {
+  for (let i = 0; i < 100; i++) {
+    createOrb(cw / 2, ch / 2 + i * 2);
+  }
 }
 
-var loop = function(){
+initOrbs();
+
+function loop() {
   window.requestAnimFrame(loop);
-	if(trail){
-		ctx.fillStyle = 'rgba(0,0,0,.1)';
-		ctx.fillRect(0,0,cw,ch);
-	} else {
-		ctx.clearRect(0,0,cw,ch);
-	}
-	var i = orbs.length;
-	while(i--){	
-		var orb = orbs[i];	
-		var updateCount = 3;
-		while(updateCount--){
-		orb.update();		
-		orb.draw(ctx);
-		}
-		
-	}
+  if (trail) {
+    ctx.fillStyle = 'rgba(0,0,0,0.1)';
+    ctx.fillRect(0, 0, cw, ch);
+  } else {
+    ctx.clearRect(0, 0, cw, ch);
+  }
+
+  orbs.forEach((orb) => {
+    orb.update();
+    orb.draw();
+  });
+
+  particles = particles.filter((particle) => particle.life > 0);
+  particles.forEach((particle) => {
+    particle.update();
+    particle.draw();
+  });
 }
-            
+
 loop();
